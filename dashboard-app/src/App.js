95e6a6d25ec827102f9da648b943ebf479f39e91
Ashish2194 <ashishsarkar94@gmail.com>
2022-10-01 15:04:52 +0530

    Added alert box

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [flag, setFlag] = useState(false);
   return (
     <div className="App">
       This is My Dashboard
+      <button onClick={() => setFlag(prev=>!prev)}>Hello World</button>
+      {flag && <h1>Hey Man!</h1>}
     </div>
   );
 }
