67e349d730df96ce4e54429fa62b4665b9cc5829
Ashish Sarkar <ashishsarkar9421@gmail.com>
2025-10-04 20:19:04 +0530

    add unit tests

--- dashboard-app/src/App.test.js
+++ dashboard-app/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders dashboard title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/This is My Dashboard/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('shows greeting when button is clicked', () => {
+  render(<App />);
+  const button = screen.getByText(/Hello World/i);
+  button.click();
+  const greeting = screen.getByText(/Hey Man!/i);
+  expect(greeting).toBeInTheDocument();
 });
